4106d70eb36d0e6e6289f8646d81b2cefaf54d75
jahids <evanjahid321@gmail.com>
2025-06-24 01:26:30 -0700

    Update app.json to change icon and splash image to logo; enhance index component with background image and logo display in ScrollView.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
 import React from 'react'
+import { images } from '../../constants/images';
+import { icons } from '@/constants/icons';
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View className='flex-1 bg-primary'>
+      <Image source={images.bg}
+        className='absolute w-full z-0'
+        resizeMode='cover'
+      />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: '100%',
+          paddingBottom: 10,
+        }}
+        className='flex-1 px-5'>
+         <Image source={icons.logo} className=' w-12 h-10 mt-20 mb-5 mx-auto' />
+      </ScrollView>
     </View>
   )
 }
