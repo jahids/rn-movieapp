66856c8e9e5744ba403b89fb2ac4479cca8cf707
jahids <evanjahid321@gmail.com>
2025-06-23 22:27:14 -0700

    first commit

--- app/(tabs)/onboarding.tsx
+++ app/(tabs)/onboarding.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const index = () => {
+const Onboarding = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text>Onboarding Screen</Text>
     </View>
   )
 }
 
-export default index
+export default Onboarding
